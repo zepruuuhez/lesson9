dbeeeb43bdff48a408d233cc98dc03e4dfc09aea
zepruuuhez <57395202+zepruuuhez@users.noreply.github.com>
2019-11-07 18:51:40 +0700

    07/11

--- lesson9/lesson9/Form1.Designer.cs
+++ lesson9/lesson9/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(202, 31);
+            this.button1.Location = new System.Drawing.Point(202, 32);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(248, 45);
             this.button1.TabIndex = 0;
@@ -50,11 +51,22 @@
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -67,6 +79,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- lesson9/lesson9/Form1.cs
+++ lesson9/lesson9/Form1.cs
@@ -22,28 +22,93 @@ namespace lesson9
             richTextBox1.Text = "";
             Random R = new Random();
             int[,] mas = new int[5, 3];
-            int max = 0, flag = 0,summa=0;
-            for(int i=0;i<5;i++)
+            int max = 0, flag = 0, summa = 0;
+            for (int i = 0; i < 5; i++)
             {
                 summa = 0;
-                for(int j = 0;j<3;j++)
+                for (int j = 0; j < 3; j++)
                 {
                     mas[i, j] = R.Next(10, 100);
                     summa += mas[i, j];
                     richTextBox1.Text += mas[i, j] + " ";
                 }
-                if(summa>max)
+                if (summa > max)
                 {
                     max = summa;
                     flag = i;
                 }
-                richTextBox1.Text += "\n";            
+                richTextBox1.Text += "\n";
             }
-            richTextBox1.Text += "\nМаксимальная сумма в " + (flag+1) + "строке" + "\n";
-            for(int j=0;j<3;j++)
+            richTextBox1.Text += "\nМаксимальная сумма в " + (flag + 1) + "строке" + "\n";
+            for (int j = 0; j < 3; j++)
             {
-                richTextBox1.Text += mas[flag, j]+" ";
+                richTextBox1.Text += mas[flag, j] + " ";
             }
+
+
+
+
+
+
+            for (int j = 0; j < 3; j++)
+            {
+                summa = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    summa += mas[i, j];
+
+                }
+                if (summa > max)
+                {
+                    max = summa;
+                    flag = j;
+                }
+
+            }
+            richTextBox1.Text += "\nМаксимальная сумма в " + (flag + 1) + " столбце" + "\n";
+            for (int j = 0; j < 5; j++)
+            { richTextBox1.Text += mas[j, flag] + "\n"; }
+
+
+
+
+
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            Random R = new Random();
+            int[,] mas = new int[5, 5];
+            int summa = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                summa = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    mas[i, j] = R.Next(10, 100);
+                    richTextBox1.Text += mas[i, j] + " ";
+
+                }
+                richTextBox1.Text += "\n";
+                
+
+            }
+            summa = 0;
+            for(int i=0;i<5;i++)
+            {
+                summa += mas[i, i];
+                
+
+            }
+            richTextBox1.Text += "\nСумма - "+ summa;
+
+            summa = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                summa += mas[i,4-i];
+            }
+            richTextBox1.Text += "\nСумма -" + summa;
+        } 
     }
 }
